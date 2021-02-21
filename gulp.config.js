5d37d7766a9a44ac32aa1db6f1bc60ff837e046c
innadeveloper <privatpost007@gmail.com>
2021-02-21 09:17:08 +0100

    Подключение библиотеки fancybox

--- gulp.config.js
+++ gulp.config.js
@@ -4,12 +4,13 @@ module.exports = {
     STYLE_LIBS : [
         'node_modules/normalize.css/normalize.css',
         'node_modules/bxslider/dist/jquery.bxslider.css',
-        'node_modules/jquery.fancybox/source/jquery.fancybox.css'
+        //'node_modules/jquery.fancybox/source/jquery.fancybox.css'
     ],
     JS_LIBS : [
+        //'node_modules/jquery/dist/jquery.js',
         'node_modules/mobile-detect/mobile-detect.js',
         'node_modules/jquery-touchswipe/jquery.touchSwipe.js',
         'node_modules/bxslider/dist/jquery.bxslider.js',  
-        'node_modules/jquery.fancybox/source/jquery.fancybox.js'    
+        //'node_modules/jquery.fancybox/source/jquery.fancybox.js'    
     ]
 }
