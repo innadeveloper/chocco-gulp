4f0f96cb56ad50dc4cf2080e082e86bdaa125958
innadeveloper <privatpost007@gmail.com>
2021-02-21 08:59:38 +0100

    Подключение библиотеки JQuery

--- gulp.config.js
+++ gulp.config.js
@@ -7,7 +7,6 @@ module.exports = {
         'node_modules/jquery.fancybox/source/jquery.fancybox.css'
     ],
     JS_LIBS : [
-        'node_modules/jquery/dist/jquery.js',
         'node_modules/mobile-detect/mobile-detect.js',
         'node_modules/jquery-touchswipe/jquery.touchSwipe.js',
         'node_modules/bxslider/dist/jquery.bxslider.js',  
